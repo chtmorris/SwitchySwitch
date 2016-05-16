9feffa2967f408bc9661bd6b84ebb8045fa6c5da
chtmorris <agoosester@gmail.com>
2016-05-16 16:47:22 +0800

    Initial Commit

---
+++ SwitchySwitch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwitchySwitch
+//
+//  Created by Charlie Morris on 16/05/2016.
+//  Copyright © 2016 Mind Fund Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
